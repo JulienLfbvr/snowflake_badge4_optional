044f00a5b1b854342d9855fc0c3a27ced34c1766
JulienLfbvr <90457257+JulienLfbvr@users.noreply.github.com>
2024-04-15 17:51:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,9 @@ st.write(
     """
 )
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
-my_cur = my_cnx.cursor() 
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_cur = cnx.cursor() 
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") 
 my_data_row = my_cur.fetchone() 
 st.text("Hello from Snowflake:") 
